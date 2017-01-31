24be60e1c8f2f62555be50045af05504df8caee3
BubDZombie <phatbudsj@yahoo.com>
2017-01-30 22:19:10 -0500

    Beginnings of boto3, limit results to today.

--- lambda/get_weather.py
+++ lambda/get_weather.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+import boto3
+import datetime
 import json
 import os
 import urllib2
 
 def lambda_handler(event, context):
-	state = 'NJ'
-	city = 'Fair_Lawn'
-	highlights = ['7:00 AM', '12:00 PM', '6:00 PM']
+	return(read_briefing({'state': 'NJ', 'city': 'Fair_Lawn', 'highlights': ['7:00 AM', '12:00 PM', '6:00 PM', '11:00 PM']}))
+
+def read_briefing(config):
+	state = config['state']
+	city = config['city']
+	highlights = config['highlights']
 
 	# Get the hourly weather forecast from the Weather Underground API.
 	url = 'http://api.wunderground.com/api/{key}/hourly/q/{state}/{city}.json'.format(
@@ -18,9 +23,10 @@ def lambda_handler(event, context):
 	wunderground_response = json.loads(wunderground_response.read())
 
 	# Get details for each highlight time.
+	today = wunderground_response['hourly_forecast'][0]['FCTTIME']['mday']
 	highlight_responses = []
 	for hour in wunderground_response['hourly_forecast']:
-		if(hour['FCTTIME']['civil'] in highlights):
+		if(hour['FCTTIME']['civil'] in highlights and hour['FCTTIME']['mday'] == today):
 			highlight_responses.append('{time}: {temp} and {condition}.'.format(
 				time=hour['FCTTIME']['civil'],
 				temp=hour['feelslike']['english'],
@@ -38,6 +44,16 @@ def lambda_handler(event, context):
 		}
 	}
 	return(alexa_response)
+
+def get_config(user_id):
+	dynamo = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url='dynamodb.us-east-1.amazonaws.com')
+	table = dynamo.Table('weather_settings')
+	try:
+		response = table.get_item(Key=user_id)
+	except ClientError as e:
+		return({'city': None, 'state': None, 'highlights': []})
+	else:
+		return(response['Item'])
 
 if(__name__ == '__main__'):
 	print('{0}'.format(lambda_handler(None, None)))
